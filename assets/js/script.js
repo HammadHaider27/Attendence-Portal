28bbcb3d3be70a93dc9c287aca515bf703d21799
HammadHaider27 <haiderhammad251@gmail.com>
2022-11-12 20:41:02 +0500

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,14 @@ import { doc, setDoc, getDoc, getFirestore, collection } from "https://www.gstat
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArIJ5K49kSBWbkTCjYmHO1BHSlefqr76U",
-  authDomain: "practice-ce932.firebaseapp.com",
-  databaseURL: "https://practice-ce932-default-rtdb.firebaseio.com",
-  projectId: "practice-ce932",
-  storageBucket: "practice-ce932.appspot.com",
-  messagingSenderId: "940873466796",
-  appId: "1:940873466796:web:03299d72276396c1ac3009",
-  measurementId: "G-1CR3SXDNQX"
-};
+    apiKey: "AIzaSyAqPqxWhK8ShP33-QxwCrOR-2Nq8FWvdJY",
+    authDomain: "hackathon-3bd89.firebaseapp.com",
+    projectId: "hackathon-3bd89",
+    storageBucket: "hackathon-3bd89.appspot.com",
+    messagingSenderId: "279187745842",
+    appId: "1:279187745842:web:7893c1a37b73ab541ee02b",
+    measurementId: "G-FPQB7ZJQHM"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -21,51 +20,139 @@ const db = getFirestore();
 const database = getDatabase();
 
 // Login
-
 let login_btn = document.getElementById("login_btn");
-
 login_btn.addEventListener("click", () => {
-
-  event.preventDefault();
+  console.log("login run");
+   event.preventDefault();
 
   let login_email = document.getElementById("login_email");
   let login_password = document.getElementById("login_password");
 
   signInWithEmailAndPassword(auth, login_email.value, login_password.value)
-    .then(async (userCredential) => {
+    .then((userCredential) => {
       // Signed in 
-      const user = userCredential.user;
-      console.log("user=>", user)
-
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-
-        let bioData = document.getElementById("bioData");
-        console.log("Document data:", docSnap.data());
-
-        const querySnapshot = await getDocs(collection(db, "users"));
-        querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-        });
-
-        bioData.innerHTML = querySnapshot();
-
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-
-      alert("Successfully Registered");
-      window.location.href = "profile.html";
-
+      console.log("userCredential", userCredential)
+      alert("Successfully Login");
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log("error=>", error)
     });
-
-
 })
+
+// add course
+let add_course_button = document.getElementById("add_course_button");
+add_course_button.addEventListener('click', async () => {
+  event.preventDefault();
+
+  let course_name = document.getElementById("course_name");
+
+  try {
+    await setDoc(doc(db, "courses"), {
+      course_name: course_name.value,
+    });
+  } catch (error) {
+    console.log("add_course_button error=>", error)
+
+  }
+})
+
+// add class
+let add_class_button = document.getElementById("add_class_button");
+add_class_button.addEventListener('click', async () => {
+  event.preventDefault();
+
+  let class_name = document.getElementById("class_name");
+  let class_timing = document.getElementById("class_timing");
+  let timing = document.getElementById("timing");
+  let teachername = document.getElementById("teachername");
+  let section = document.getElementById("section");
+  let course = document.getElementById("course");
+
+
+  try {
+    await setDoc(doc(db, "courses"), {
+      course_name: class_name.value,
+      class_timing: class_timing.value,
+      timing: timing.value,
+      teachername: teachername.value,
+      section: section.value,
+      course: course.value,
+    });
+  } catch (error) {
+    console.log("add_course_button error=>", error)
+
+  }
+})
+
+// add student
+let add_student_button = document.getElementById("add_student_button");
+add_student_button.addEventListener('click', async () => {
+  event.preventDefault();
+
+  let student_name = document.getElementById("name");
+  let fathername = document.getElementById("fathername");
+  let rollnum = document.getElementById("rollnum");
+  let phone = document.getElementById("phone");
+  let cnic = document.getElementById('cnic')
+  let course = document.getElementById('course')
+  let gender = document.getElementById('gender');
+
+  const student_pic = document.getElementById("file");
+  const imageSrc = await changingImageToURL(student_pic.files[0],user.uid);
+
+  try {
+    await setDoc(doc(db, "courses"), {
+      name: student_name.value,
+      student_image: imageSrc,
+      fathername: fathername.value,
+      rollnum: rollnum.value,
+      phone: phone.value,
+      cnic: cnic.value,
+      course: course.value,
+      gender: gender.value
+    });
+  } catch (error) {
+    console.log("add_course_button error=>", error)
+
+  }
+})
+
+// Image uploader
+const changingImageToURL = (file) => {
+  return new Promise((resolve, reject) => {
+    const storageRef = ref(storage, `images/${file?.name}.png`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        switch (error.code) {
+          case 'storage/unauthorized':
+            break;
+          case 'storage/canceled':
+            break;
+          case 'storage/unknown':
+            break;
+        }
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+         resolve(downloadURL)
+        });
+      }
+    );
+  })
+}
